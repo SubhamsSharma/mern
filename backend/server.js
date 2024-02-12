478716453ead547461fa6649c00c0c0232cc3ff5
Shubham Sharma <iamsubham63@gamil.com>
2024-02-12 12:25:46 +0530

    add routes dir

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,11 @@
-console.log("Hello World ")
+const express = require("express")
+const dotenv = require("dotenv").config()
+
+const port = process.env.PORT || 8000
+const app = express()
+
+app.get("/api/goals", (req,res) => {
+    res.status(200).json({message:"all the goals"})
+})
+
+app.listen(port, () => console.log(`server listening at port ${port}`))
